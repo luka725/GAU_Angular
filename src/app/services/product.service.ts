c5d35ba7b17beebce864b320373e4c635f6d48ba
luka <luka.sheylashvili@gmail.com>
2024-04-08 02:40:22 +0400

    add

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
-
+interface Product{
+  id:number,
+  name:string,
+  rate:number,
+  price:number,
+  oldPrice:number,
+  imgUrl:string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
   constructor() { }
+
+  products:Product[] = [
+    { id: 1, name: "Dates Value Pack Pouch", rate: 4, price: 120.25, oldPrice: 123.25, imgUrl: "asd"},
+    { id: 2, name: "Smoked Honey Spiced Nuts", rate: 4, price: 120.25, oldPrice: 123.25, imgUrl: "asdasd"}
+  ]
+
+  getCartProducts(){
+    return this.products;
+  }
 }
  
