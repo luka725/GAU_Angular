26288315655d721e68182fffd31326b2fa7fdd3f
luka <luka.sheylashvili@gmail.com>
2024-05-20 19:28:11 +0400

    adding router link at header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
-
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
