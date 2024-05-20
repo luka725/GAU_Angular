ff5dad404d34860b8586c453c428e773cdb32edc
luka <luka.sheylashvili@gmail.com>
2024-05-20 19:21:35 +0400

    adjusting styles for register and login components

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      // Your registration logic here
+      console.log(this.registerForm.value);
+    }
+  }
 }
