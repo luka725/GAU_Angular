39bd5a17dce5ac2a1e0c6ed7eb8ae616414916f1
luka <luka.sheylashvili@gmail.com>
2024-05-20 18:00:34 +0400

    generating footer, header, home, login and register components adjusting them on the screen and adding user service in the application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "login", component: LoginComponent},
+    { path: "register", component: RegisterComponent}
+];
---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+
+interface User{
+  firstname:string,
+  lastname:string,
+  username:string,
+  password:string,
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  users:User[] = [
+    {
+      firstname: "Luka",
+      lastname: "Sheklashvili",
+      username: "LS",
+      password: "123",
+    },
+    {
+      firstname: "Lukas",
+      lastname: "Sh",
+      username: "LSh",
+      password: "1234",
+    },
+  ]
+
+}
